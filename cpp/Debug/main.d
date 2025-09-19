987f616f474478e8fa5e1a907130235552fb0765
Henke <henkenyholm@gmail.com>
2025-09-19 10:55:52 +0200

    initial commit

---
+++ cpp/Debug/main.d
@@ -0,0 +1,61 @@
+main.d main.o: .././main.cpp \
+ ../driver/atmega328p/include/driver/atmega328p/adc.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ ../driver/include/driver/adc/interface.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ ../driver/atmega328p/include/driver/atmega328p/eeprom.h \
+ ../driver/include/driver/eeprom/interface.h \
+ ../utils/include/utils/type_traits.h \
+ ../driver/atmega328p/include/driver/atmega328p/gpio.h \
+ ../driver/include/driver/gpio/interface.h \
+ ../driver/atmega328p/include/driver/atmega328p/serial.h \
+ ../driver/include/driver/serial/interface.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ ../driver/atmega328p/include/driver/atmega328p/timer.h \
+ ../driver/include/driver/timer/interface.h \
+ ../driver/atmega328p/include/driver/atmega328p/watchdog.h \
+ ../driver/include/driver/watchdog/interface.h \
+ ../target/include/target/system.h
+
+../driver/atmega328p/include/driver/atmega328p/adc.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+../driver/include/driver/adc/interface.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+../driver/atmega328p/include/driver/atmega328p/eeprom.h:
+
+../driver/include/driver/eeprom/interface.h:
+
+../utils/include/utils/type_traits.h:
+
+../driver/atmega328p/include/driver/atmega328p/gpio.h:
+
+../driver/include/driver/gpio/interface.h:
+
+../driver/atmega328p/include/driver/atmega328p/serial.h:
+
+../driver/include/driver/serial/interface.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+../driver/atmega328p/include/driver/atmega328p/timer.h:
+
+../driver/include/driver/timer/interface.h:
+
+../driver/atmega328p/include/driver/atmega328p/watchdog.h:
+
+../driver/include/driver/watchdog/interface.h:
+
+../target/include/target/system.h:
---
+++ cpp/Debug/makedep.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+driver\atmega328p\source\adc.cpp
+
+driver\atmega328p\source\eeprom.cpp
+
+driver\atmega328p\source\gpio.cpp
+
+driver\atmega328p\source\serial.cpp
+
+driver\atmega328p\source\timer.cpp
+
+driver\atmega328p\source\watchdog.cpp
+
+main.cpp
+
+target\source\system.cpp
+
+utils\source\utils.cpp
+
